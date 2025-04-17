a1c676ca51c531932ba18256562c99cff9ee6a46
JaeJune Ryu <ryungom@gmail.com>
2025-04-18 02:09:29 +0900

    [Silver V] Title: 브실이의 입시전략, Time: 204 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.js
+++ 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.js
@@ -3,38 +3,41 @@ let input = require("fs")
   .toString()
   .split("\n")
   .map((line) => line.replace("\r", "").split(" "));
-const [nums, ...rest] = input;
-const subject = rest.slice(0, Number(nums[0]));
-const must = rest.slice(Number(nums[0], nums[0] + nums[2]));
 
-function solution(nums, subject, must) {
-  const sort = subject.sort((a, b) => a[1] - b[1]);
+// input 들어오는거 싹 정리
+const [nums, ...rest] = input;
+const [K, M, N] = nums.map(Number);
+const subject = rest.slice(0, K);
+const must = rest.slice(Number(K, K + N));
+
+function solution(K, M, N, subject, must) {
+  const sort = subject.sort((a, b) => Number(a[1]) - Number(b[1]));
   let min = 0;
   let max = 0;
+
+  // 맵 할당해서 현 기준 필수과목 점수에 넣어두고, 필수과목 항목 제외
   const arr = new Map();
   sort.forEach((value) => {
-    arr.set(value[0], value[1]);
+    arr.set(value[0], Number(value[1]));
   });
   must.forEach((value) => {
     if (arr.has(value[0])) {
-      min += Number(arr.get(value[0]));
-      max += Number(arr.get(value[0]));
+      min += arr.get(value[0]);
+      max += arr.get(value[0]);
       arr.delete(value[0]);
     }
   });
 
   // 최소, 최대값 구하기
   const setArr = Array.from(arr);
-
-  let viewNum = nums[1] - nums[2];
+  let viewNum = M - N;
   for (let i = 0; i < viewNum; i++) {
-    min += Number(setArr[i][1]);
+    min += setArr[i][1];
   }
-  for (let i = nums[0] - nums[2]; i > nums[0] - nums[2] - viewNum; i--) {
-    max += Number(setArr[i - 1][1]);
+  for (let i = K - N; i > K - N - viewNum; i--) {
+    max += setArr[i - 1][1];
   }
-
   return console.log(min, max);
 }
 
-solution(nums, subject, must);
+solution(K, M, N, subject, must);
