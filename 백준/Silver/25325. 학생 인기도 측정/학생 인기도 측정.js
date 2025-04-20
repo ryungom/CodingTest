f132b091b1a4139b13f0b5bd27f05e0095b18dd4
JaeJune Ryu <ryungom@gmail.com>
2025-04-20 23:53:20 +0900

    [Silver V] Title: 학생 인기도 측정, Time: 168 ms, Memory: 10344 KB -BaekjoonHub

---
+++ 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.js
@@ -0,0 +1,33 @@
+let input = require("fs")
+  .readFileSync(0, "utf-8")
+  .toString()
+  .split("\n")
+  .map((line) => line.replace("\r", "").split(" "));
+
+// input 들어오는거 싹 정리
+const [nums, kindStu, ...rest] = input;
+const kindNum = Number(nums[0]);
+const lovers = rest.slice(0, kindNum);
+
+function solution(kindNum, kindStu, lovers) {
+  const result = new Map();
+  kindStu.forEach((value) => result.set(value, 0));
+
+  for (let value of result.keys()) {
+    let sum = 0;
+    lovers.forEach((value2) => {
+      if (value2.includes(value)) {
+        sum++;
+        result.set(value, sum);
+      }
+    });
+  }
+
+  const sort = Array.from(result)
+    .sort((a, b) => b[1] - a[1])
+    .map((item) => console.log(item[0], item[1]));
+
+  return sort;
+}
+
+solution(kindNum, kindStu, lovers);
