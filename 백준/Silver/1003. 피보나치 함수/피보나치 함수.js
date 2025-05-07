a895f5c657571880f98a9acbc4f8528180e4020d
JaeJune Ryu <ryungom@gmail.com>
2025-05-08 03:27:07 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 9880 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, 'utf-8').toString().trim().split("\n").map(Number);
+const problem = input.slice(1);
+
+class Fibo {
+  constructor() {
+    this._cache = {};
+  }
+  memo(value) {
+    if (this._cache[value] !== undefined) return this._cache[value];
+    if (value === 0) return 0;
+    if (value <= 2) return 1;
+
+    this._cache[value] = this.memo(value - 1) + this.memo(value - 2);
+    return this._cache[value];
+  }
+}
+const result = new Fibo();
+
+for (let i of problem) {
+  if (i === 0) console.log(1, 0);
+  else console.log(result.memo(i - 1), result.memo(i));
+}
