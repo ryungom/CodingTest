90d6fa8845ee751af53e7d5d4a79c2a82d84bc3a
JaeJune Ryu <ryungom@gmail.com>
2025-05-14 23:05:08 +0900

    [Bronze II] Title: 수 정렬하기, Time: 100 ms, Memory: 9704 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 const input = fs.readFileSync(0,"utf-8").toString().trim().split("\n").map(Number);
 
 const num = input[0];
-const arr = input.slice(1, input.length).sort((a, b) => a - b);
-for (let i of arr) {
-  console.log(i);
-}
+const arr = input
+  .slice(1, input.length)
+  .sort((a, b) => a - b)
+  .join("\n");
+console.log(arr)
